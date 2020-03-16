4d346709f7f78d361a4d7845c84ba1bdb3079d8d
PiskoOo <piskooooooo@gmail.com>
2020-03-16 16:13:40 +0000

    Updates

---
+++ precache-manifest.97728270c6de7a7e8aa7e14e2a490ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3571f6a27b1fffe6f6b600dc86603ed8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d6f207bc9e7be8458fb5",
+    "url": "/monsters-rolodex/static/css/main.913d09c4.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f207bc9e7be8458fb5",
+    "url": "/monsters-rolodex/static/js/main.e56e4ccb.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
